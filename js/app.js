e04decb634c4d195c46477eafef406627ef986b3
bigot-x@hotmail.com <bigot-x@hotmail.com>
2015-12-08 22:21:17 -0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+angular.module('myApp', ['angular-table'])
+	.controller("ProdsCtrl", ["$scope", "$http", function($scope, $http) {
+	
+		var vm = $scope;
+		vm.products = [];
+	
+		vm.loadProducts = function() {
+			var httpRequest = $http({
+				method: 'GET',
+				url: 'https://github.com/msarmi/tabi2015.git/clean_table_big.json',
+				data: ''
+			}).success(function(data, status) {
+				vm.products = data;
+				vm.total_productos = data.length;
+			});
+		};
+		
+		vm.config_at = {
+			itemsPerPage: 50,
+			fillLastPage: true
+		};
+		
+		vm.loadProducts();
+
+}]);
